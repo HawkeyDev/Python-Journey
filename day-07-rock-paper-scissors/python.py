21d202550ec1e761b098b6dd7c49541bd03fd0d3
Vetrivel <j.vetrivel77@gmail.com>
2026-06-10 22:45:51 +0530

    day 07 - rock paper scissors

--- day-07-rock-paper-scissors/python.py
+++ day-07-rock-paper-scissors/python.py
@@ -6,10 +6,26 @@ while True:
     computer = random.choice(choices)
     guess = input("Enter rock, paper, or scissors: ")
 
-    if guess == computer:
-        print("Your choice is right")
-    else:
-        print("Your guess was wrong")
-
+    if guess == "rock" and computer == "scissors":
+        print("You win!")
+    elif  guess == "paper" and computer == "scissors":
+        print("You lose!")
+    elif guess == "scissors" and computer == "scissors":
+        print("Draw")
+    elif guess == "rock" and computer == "rock":
+        print("Draw")
+    elif guess == "paper" and computer == "rock":
+        print("You win!")
+    elif guess == "scissors" and computer == "rock":
+        print("You lose!")
+    elif guess == "rock" and computer == "paper":
+        print("You lose!")
+    elif guess == "paper" and computer == "paper":
+        print("Draw!")
+    elif guess == "scissors" and computer == "paper":
+        print("You win!")
+   
     print("Computer choice:", computer)
-    
+    play = input("Play again? (yes/no): ")
+    if play == "no":
+        break
