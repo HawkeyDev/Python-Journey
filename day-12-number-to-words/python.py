f70cb7fe130dce58b64eba9c6db0bee10c62bc35
HawkeyDev <j.vetrinvel77@gmail.com>
2026-06-14 10:22:35 +0530

    day 12 - number to words

---
+++ day-12-number-to-words/python.py
@@ -0,0 +1,18 @@
+numbers ={
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten"
+}
+num = int(input("Enter a number between 0 and 10: "))
+if num in numbers:
+    print(f"The number {num} in words is: {numbers[num]}")
+else:
+    print("Number out of range.")
